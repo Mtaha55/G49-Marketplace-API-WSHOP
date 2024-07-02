2816564001204506265ed243e544953e98c7fbc0
Mohammed <mt_h22@yahoo.com>
2024-07-02 13:16:54 +0200

    Marketplace Workshop Project.

--- src/main/java/se/lexicon/g49marketplaceapiwshop/config/HelloController.java
+++ src/main/java/se/lexicon/g49marketplaceapiwshop/config/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MarketController {
+public class HelloController {
 
     @GetMapping("/api/Market")
     public String Market() {
